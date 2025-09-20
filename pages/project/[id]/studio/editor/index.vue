5acd7a8dd9730160cd3d70faf049416b594d85f8
samsonkebede4 <samsonkebede4@gmail.com>
2025-09-20 17:10:02 +0300

    Feature:Work on sql editor

--- pages/project/[id]/studio/editor/index.vue
+++ pages/project/[id]/studio/editor/index.vue
@@ -1,22 +1,37 @@
 <template>
-      <div class="items-center">
+      <div class="flex flex-col gap-4">
     <h1>SQL Editor</h1>
         <div >
-    <ClientOnly>
-      <CodeMirrorEditor />
+          <div class="items-center">
+            <ClientOnly>
+      <CodeMirrorEditor v-model="query"/>
 
       <template #fallback>
         <p>Loading Editor...</p>
       </template>
     </ClientOnly>
+          </div>
   </div>
+  <button @click="handleExecute" class="btn btn-primary mt-4 justify-end">Run</button>
   </div>
+      
 </template>
 
 <script setup>
+import axios from 'axios';
 definePageMeta({
   layout: "studio",
 });
+const query = ref(null)
+const handleExecute = async () => {
+  console.log('Executing Query:', query.value);
+  try {
+    const response = await axios.post('/query/execute', {"query":query});
+    console.log('Query Result:', response.data);
+  } catch (error) {
+    console.error('Error executing query:', error);
+  }
+}
 </script>
 
 <style>
